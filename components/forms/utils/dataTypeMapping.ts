117f04bc43a5176c0377ed27cae9bd2edf669a84
Matt Wedge <matthew.wedgeroberts@gmail.com>
2022-10-26 18:23:09 -0500

    submit form data from boat form

--- components/forms/utils/dataTypeMapping.ts
+++ components/forms/utils/dataTypeMapping.ts
@@ -3,5 +3,6 @@ import { FormDataType } from '../RTKForm';
 export const dataTypeMapping = {
   Pet: 'form',
   Car: 'form',
+  Boat: 'form',
   default: 'json'
 } as { [key: string]: FormDataType | undefined };
