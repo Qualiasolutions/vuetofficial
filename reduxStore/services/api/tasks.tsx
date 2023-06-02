5f29be9f7add9964df379d4ca2c935ab0d5e554a
Matt Wedge <matthew.wedgeroberts@gmail.com>
2023-06-02 13:58:23 -0300

    add fixed task performance

--- reduxStore/services/api/tasks.tsx
+++ reduxStore/services/api/tasks.tsx
@@ -165,7 +165,69 @@ const tasksApi = vuetApi.injectEndpoints({
           body
         };
       },
-      invalidatesTags: ['Task']
+      invalidatesTags: ['Task'],
+      async onQueryStarted(
+        { ...patch },
+        { dispatch, queryFulfilled, getState }
+      ) {
+        try {
+          const { data: newTask } = await queryFulfilled;
+          for (const {
+            endpointName,
+            originalArgs
+          } of tasksApi.util.selectInvalidatedBy(getState(), [
+            { type: 'Task' }
+          ])) {
+            if (!['getAllTasks', 'getAllScheduledTasks'].includes(endpointName))
+              continue;
+            if (endpointName === 'getAllTasks') {
+              dispatch(
+                tasksApi.util.updateQueryData(
+                  'getAllTasks',
+                  originalArgs,
+                  (draft) => {
+                    draft.ids.push(newTask.id);
+                    draft.byId[newTask.id] = {
+                      ...newTask,
+                      resourcetype: 'FixedTask'
+                    };
+                  }
+                )
+              );
+            }
+            if (endpointName === 'getAllScheduledTasks') {
+              // We can only make these performant updates if the task is
+              // not recurrent - otherwise we simply have to wait for
+              // the server response
+              if (!newTask.recurrence) {
+                dispatch(
+                  tasksApi.util.updateQueryData(
+                    'getAllScheduledTasks',
+                    originalArgs,
+                    (draft) => {
+                      draft.byTaskId[newTask.id] = {
+                        '-1': {
+                          ...newTask,
+                          recurrence: null, // Flexible tasks never recurrent
+                          recurrence_index: null, // Flexible tasks never recurrent
+                          alert: [], // Assume no alert - this will update when data is refetched
+                          resourcetype: 'FixedTask'
+                        }
+                      };
+                      draft.ordered.push({
+                        id: newTask.id,
+                        recurrence_index: null
+                      });
+                    }
+                  )
+                );
+              }
+            }
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      }
     }),
     createFlexibleFixedTask: builder.mutation<
       FixedTaskResponseType,
