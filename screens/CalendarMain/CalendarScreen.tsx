3e3702cc63dcd43f864b8642f6d470c3ba6ff8a4
Matt Wedge <matthew.wedgeroberts@gmail.com>
2023-06-06 14:56:22 -0300

    spinner when loading

--- screens/CalendarMain/CalendarScreen.tsx
+++ screens/CalendarMain/CalendarScreen.tsx
@@ -1,6 +1,8 @@
 import Calendar from 'components/calendars/TaskCalendar';
+import { FullPageSpinner } from 'components/molecules/Spinners';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
+import { useGetAllScheduledTasksQuery } from 'reduxStore/services/api/tasks';
 import { selectFilteredScheduledTaskIdsByDate } from 'reduxStore/slices/calendars/selectors';
 
 const styles = StyleSheet.create({
@@ -13,6 +15,11 @@ const styles = StyleSheet.create({
 
 export default function CalendarScreen() {
   const filteredTasks = useSelector(selectFilteredScheduledTaskIdsByDate);
+  const { isLoading } = useGetAllScheduledTasksQuery();
+
+  if (isLoading) {
+    return <FullPageSpinner />;
+  }
 
   return (
     <SafeAreaView style={styles.container}>
